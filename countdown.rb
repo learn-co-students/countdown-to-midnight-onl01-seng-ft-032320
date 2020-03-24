176a7884dca843c4d91c9df6d1cc775f61754040
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 21:06:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sec)
+  while sec > 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
 end
