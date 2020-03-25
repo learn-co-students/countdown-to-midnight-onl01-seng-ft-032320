4ca2d292aefceb7db3260016da861ecb36d80fa9
edmundo papi <edmundopierre@yahoo.com>
2020-03-25 01:14:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
+require 'pry'
+def countdown(number)
 
-def countdown
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -=1
+    #binding.pry
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+
+  while number >= 1
+    sleep 2
+    puts "#{number} SECOND(S)!"
+    number -=1
+    #binding.pry
+  end
+
 end
